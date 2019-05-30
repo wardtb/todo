c5c0a409b612aaf5a81729019674ea2c4991ca9b
Tom Ward <wardtb@gmail.com>
2019-05-29 22:07:41 -0700

    adding initial template structure

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,26 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Tom'}
+    items = [
+        {
+            'id': 1,
+            'name': 'grocery store',
+            'category': 'chores'
+        },
+        {
+            'id': 2,
+            'name': 'workout',
+            'category': 'health'
+        },
+        {
+            'id': 3,
+            'name': 'run',
+            'category': 'health'
+        }
+
+    ]
+    return render_template('index.html', title='Home', user=user, items=items)
