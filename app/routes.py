12d2103913ee63d5db0ce141495e3151561ddbcb
Tom Ward <wardtb@gmail.com>
2019-06-10 22:47:37 -0700

    adding web forms and WTForms

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,9 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
+
 @app.route('/index')
 def index():
     user = {'username': 'Tom'}
@@ -24,3 +26,12 @@ def index():
 
     ]
     return render_template('index.html', title='Home', user=user, items=items)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
